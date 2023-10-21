3425e307f1d6c0d852ccfeec1a47ddf4a62cc2ba
EddieHandford <47256888+EddieHandford@users.noreply.github.com>
2023-10-21 20:24:25 +0100

    Float
    To do :
    
    Add method of Debug
    Add pressure sensor read

--- Pico_Power/src/main.cpp
+++ Pico_Power/src/main.cpp
@@ -12,4 +12,7 @@ void loop() {
   delay(1000);               // wait for a second
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
   delay(1000);               // wait for a second
+
+//float
+
 }
